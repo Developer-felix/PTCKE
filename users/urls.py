a4a6be8b4dae8ba791e8f996e0fc09191b0b696a
Developer-felix <onjombafelix1@gmail.com>
2022-05-23 07:13:19 -0700

    Sucess Wallet

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.views import login_user, login_user,register,otp,reset_password,parent_dashboard,child_dashboard,splash,landing,logout_view
-from wallet.views import top_up
+from wallet.views import top_up, top_up_success
 
 app_name = 'users'
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('reset_password/', reset_password,name='ptc-reset_password'),
     path('parent/dashboard/', parent_dashboard,name='ptc_parent_dashboard'),
     path('wallet/top_up/', top_up,name='ptc_wallet_top_up'),
+    path('wallet/top_up/success/',top_up_success,name="ptc-wallet_top_up_success"),
 
     path('child/dashboard/', child_dashboard,name='ptc_child_dashboard'),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from config.PTC_MPESA.lipa_na_mpesa_online import lipa_na_mpesa
 
 from wallet.models import Wallet
 
@@ -7,6 +8,8 @@ def top_up(request):
     if request.method == "POST":
         ammount = request.POST.get("ammount")
         phone = request.POST.get("phone")
+
+        # lipa_na_mpesa(phone_number=254713303092,ammount="1")
 
         print(ammount)
         def get_wallet_balance():
@@ -21,9 +24,22 @@ def top_up(request):
         Wallet.objects.filter(user_id=request.user.id).update(account_balance=updated_balance)
 
         #Succes Modals
+        return redirect("success/?ammount="+ammount)
         
     return render(request,'parent/top_up.html')
 
 
 def top_up_success(request):
-    return render(request,'parent/top_up_success.html')
+    ammount = request.GET.get("ammount")
+    print(ammount)
+    def get_wallet_balance():
+            wallet = Wallet.objects.filter(user_id=request.user.id)
+            for wallet in wallet:
+                wallet_balance =  wallet.account_balance
+                return wallet_balance
+    wallet_balance=get_wallet_balance()
+    data = {
+        "ammount" : ammount,
+        "wallet_balance":wallet_balance
+    }
+    return render(request,'parent/top_up_success.html',data)
