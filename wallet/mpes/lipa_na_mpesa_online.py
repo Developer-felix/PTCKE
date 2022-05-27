be6b4810eae9f66ac223c135399dc580b55cf3fa
Developer-felix <onjombafelix1@gmail.com>
2022-05-27 04:35:07 -0700

    Deleted DB

--- wallet/mpes/lipa_na_mpesa_online.py
+++ wallet/mpes/lipa_na_mpesa_online.py
@@ -43,8 +43,8 @@ def lipa_na_mpesa(phone_number,ammount):
     response = requests.post(api_url,json=request,headers=headers)
 
 # lipa_na_mpesa(phone_number="254717713943",ammount="1")
-lnm = LNMOnline.objects.all()
-for l in lnm:
-    print(l.Amount)
+# lnm = LNMOnline.objects.all()
+# for l in lnm:
+#     print(l.Amount)
 
 
