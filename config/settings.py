3d34c2538324509330734d46f6f08937c98c5a85
Developer-felix <onjombafelix1@gmail.com>
2022-05-27 07:36:40 -0700

    changed db to ptc

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'ptc.sqlite3',
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
